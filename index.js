12926dfc84d7c159308ef5bb9b1b9a206c7e4061
yancath <catherine.yan01@gmail.com>
2020-06-19 22:39:45 -0400

    Submission for 3.2 added

--- index.js
+++ index.js
@@ -32,6 +32,14 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
   })
 
+app.get('/api/info', (request, response) => {
+    response.send(
+        `<p>Phonebook has info for ${persons.length} people</p>
+        <p>${new Date()}</p>
+        `
+    )
+  })
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
