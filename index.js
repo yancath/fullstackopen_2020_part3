e827e56106252943cd4f0c311c00a78f1aa29086
yancath <catherine.yan01@gmail.com>
2020-06-23 17:59:30 -0400

    First commit

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ let persons = [
     response.status(204).end()
   })
 
-  const PORT = 3001
+  const PORT = process.env.PORT || 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
