0696d80334fd052430112a14d112dd34aaa9aa12
yancath <catherine.yan01@gmail.com>
2020-06-27 21:12:57 -0400

    Submission for 3.17 added

--- index.js
+++ index.js
@@ -103,6 +103,21 @@ let persons = [
     .catch(error => next(error))
   })
 
+  app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+  
+    const person = {
+      name: body.name,
+      number: body.number,
+    }
+  
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+      .then(updatedPerson => {
+        response.json(updatedPerson.toJSON())
+      })
+      .catch(error => next(error))
+  })
+
   const errorHandler = (error, request, response, next) => {
     console.error(error.message)
 
