3b04660f4cde18b984f3ea57edc236b9cbf53b07
yancath <catherine.yan01@gmail.com>
2020-06-28 00:11:29 -0400

    Submission for 3.18 added

--- index.js
+++ index.js
@@ -7,32 +7,6 @@ const app = express()
 const mongoose = require('mongoose')
 const Person = require('./models/person')
 
-
-let persons = [
-    {
-      "name": "Arto Hellas",
-      "number": "040-123456",
-      "id": 1
-    },
-    {
-      "name": "Ada Lovelace",
-      "number": "39-44-5323523",
-      "id": 2
-    },
-    {
-      "name": "Dan Abramov",
-      "number": "12-43-234345",
-      "id": 3
-    },
-    {
-      "name": "Mary Poppendieck",
-      "number": "39-23-6423122",
-      "id": 4
-    }
-  ];
-
-
-
   app.use(express.json()) //parses JSON data
   app.use(cors()) //cross origin resource sharing
   app.use(express.static('build')) //for static file reading
@@ -71,10 +45,6 @@ let persons = [
     })
   })
 
-  app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-  })
-
   app.get('/api/persons', (request, response) => {
     Person.find({}).then(persons => {
       response.json(persons)
@@ -82,11 +52,13 @@ let persons = [
   })
 
   app.get('/api/info', (request, response) => {
-    response.send(
-        `<p>Phonebook has info for ${persons.length} people</p>
+    Person.find({}).then(result => {
+      response.send(
+        `<p>Phonebook has info for ${result.length} people</p>
         <p>${new Date()}</p>
         `
-    )
+      )
+    })
   })
 
   app.get('/api/persons/:id', (request, response) => {
